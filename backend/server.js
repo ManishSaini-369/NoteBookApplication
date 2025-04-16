12410d4fb4fbc54226baa91b81b6368d8315ca31
ManishSaini-369 <manishsaini1340@gmail.com>
2025-04-17 00:15:27 +0530

    corse setting

--- backend/server.js
+++ backend/server.js
@@ -41,6 +41,8 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 // app.use(cors({ origin: ["http://localhost:5173"], credentials: true }))
+app.use(cors({ origin: ["https://note-book-application.vercel.app"], credentials: true }))
+
 
 
 
